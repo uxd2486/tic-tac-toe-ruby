8823e6923196d5a5cfae385abd45b167abcb16af
Utkarsh Dayal <uxd2486@rit.edu>
2022-09-18 17:11:54 +0530

    Added functionality to initialize an empty board

--- board.rb
+++ board.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
+require_relative 'square'
+
 # this class represents the entire tic-tac-toe board
 class Board
+  attr_accessor :squares
+
   def initialize
     @squares = []
+    3.times do
+      row = []
+      3.times do
+        row << Square.new
+      end
+      @squares << row
+    end
   end
 end
--- square.rb
+++ square.rb
@@ -3,6 +3,8 @@
 # this class represents a single square on the
 # tic-tac-toe board
 class Square
+  attr_accessor :type
+
   def initialize
     @type = nil
   end
