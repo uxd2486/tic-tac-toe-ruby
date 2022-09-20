913115dfd62b56ff25295d2c8c9ff6131ded7f74
Utkarsh Dayal <uxd2486@rit.edu>
2022-09-20 17:16:39 +0530

    Added the main gameplay loop

--- game.rb
+++ game.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
+require_relative 'board'
+
 def play_game
-  nil
+  board = Board.new
+  cur_player = :X
+  puts 'Enter the square you wish to play as "[row],[column]", no spaces'
+  until board.full?
+    puts "Player #{cur_player}: "
+    input = gets.chomp.split(',')
+    row = input[0].to_i
+    column = input[1].to_i
+    result = board.set_square(row, column, cur_player)
+    next if result != cur_player
+
+    board.display
+    if board.game_won?
+      puts "#{cur_player} won!"
+      break
+    end
+    cur_player = cur_player == :X ? :O : :X
+  end
 end
--- main.rb
+++ main.rb
@@ -3,10 +3,8 @@
 require_relative 'game'
 
 def start_game
-  puts 'Would you like to play a game? \'y\' to start, \'n\' to exit'
-  answer = gets.chomp.downcase
-  play_game if answer == 'y'
-  nil
+  puts 'Let\'s start the game!'
+  play_game
 end
 
 start_game
